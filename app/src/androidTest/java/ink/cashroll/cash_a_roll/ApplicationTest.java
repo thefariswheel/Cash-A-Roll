d2472270e4c390213a74fba64bb60d336c9f9c76
faristhewheel <Faris@Fariss-iMac.attlocal.net>
2016-07-24 22:02:04 -0700

    Initial commit

---
+++ app/src/androidTest/java/ink/cashroll/cash_a_roll/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ink.cashroll.cash_a_roll;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
